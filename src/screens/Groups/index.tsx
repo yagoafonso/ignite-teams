d0b86b0a368fd3a30469e0d86dafb42a5ae1edb8
Yago Afonso <yago.afonso@hotmail.com>
2022-10-10 16:15:37 -0400

    add theme app

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,18 +1,10 @@
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Container, Title } from './styles';
 
 export function Groups() {
   return (
-    <View style={styles.container}>
-      <Text>Groups</Text>
-    </View>
+    <Container>
+      <Title>Groups</Title>
+    </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
