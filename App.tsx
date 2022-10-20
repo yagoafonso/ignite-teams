e72ee14c32d407064761fa7baf1d95c082b3273f
Yago Afonso <yago.afonso@hotmail.com>
2022-10-20 15:37:06 -0400

    create component Loading

--- App.tsx
+++ App.tsx
@@ -1,7 +1,8 @@
-import { ActivityIndicator } from 'react-native'
 import { Groups } from "@screens/Groups";
 import { ThemeProvider } from 'styled-components';
-import { useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto'
+import { useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto';
+
+import { Loading } from '@components/Loading'; 
 
 import theme from './src/theme';
 
@@ -11,7 +12,7 @@ export default function App() {
   
   return (
     <ThemeProvider theme={theme}>
-      { fontsLoaded ? <Groups /> : <ActivityIndicator /> }
+      { fontsLoaded ? <Groups /> : <Loading /> }
     </ThemeProvider>
 
   );
