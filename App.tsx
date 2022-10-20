790c74f737e49d437ebf116fbc3983d9934386ab
Yago Afonso <yago.afonso@hotmail.com>
2022-10-20 16:06:52 -0400

    create component Header

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import { StatusBar } from 'react-native';
 import { Groups } from "@screens/Groups";
 import { ThemeProvider } from 'styled-components';
 import { useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto';
@@ -12,6 +13,11 @@ export default function App() {
   
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       { fontsLoaded ? <Groups /> : <Loading /> }
     </ThemeProvider>
 
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,9 +1,11 @@
+import { Header } from '@components/Header';
 import React from 'react';
 import { Container,  } from './styles';
 
 export function Groups() {
   return (
     <Container>
+      <Header />
       
     </Container>
   );
