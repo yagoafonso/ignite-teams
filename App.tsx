32673c1c83573fa8c55d30ef7234d7520466175b
Yago Afonso <yago.afonso@hotmail.com>
2022-12-26 11:05:40 -0400

    feat: create screen new group

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'react-native';
-import { Groups } from "@screens/Groups";
+import { NewGroup } from "@screens/NewGroup";
 import { ThemeProvider } from 'styled-components';
 import { useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto';
 
@@ -18,7 +18,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      { fontsLoaded ? <Groups /> : <Loading /> }
+      { fontsLoaded ? <NewGroup /> : <Loading /> }
     </ThemeProvider>
 
   );
