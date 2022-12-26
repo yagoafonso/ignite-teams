4c57b75c74c9c3c22f87fe5ba406b3007ac17351
Yago Afonso <yago.afonso@hotmail.com>
2022-12-26 10:21:29 -0400

    feat: create component button

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,11 +4,13 @@ import { FlatList } from 'react-native';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
+import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
 
 import { Container,  } from './styles';
 
 export function Groups() {
-  const [groups, setGroups] = useState<string[]>(['Galera da Rocket']);
+  const [groups, setGroups] = useState<string[]>([]);
 
   return (
     <Container>
@@ -26,8 +28,16 @@ export function Groups() {
             title={item} 
           />
         )}
+        contentContainerStyle={groups.length === 0 && { flex: 1}}
+        ListEmptyComponent={() => (
+          <ListEmpty
+            message="Que tal cadastrar a primeira turma?" 
+          />
+        )}
       />
-
+      <Button
+        title="Criar nova turma"
+      />
     </Container>
   );
 }
